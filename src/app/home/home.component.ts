398aa76a09aa939d3f904162d67321eab49dcff4
Pankaj Phour <pankaj.phour70@gmail.com>
2023-09-13 10:44:11 +0530

    UI updated for services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  servicePoints:any = [
+    {img:'',heading:'Family Support',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.'},
+    {img:'',heading:'Health Benifits',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.'},
+    {img:'',heading:'Scholarships',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.'},
+    {img:'',heading:'Therapy',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.'},
+  ]
   constructor() { }
 
   ngOnInit(): void {
